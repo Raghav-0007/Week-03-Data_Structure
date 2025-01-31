f97a674a1f390bea617508232c3630d424f69c22
Raghav-0007 <rmyadav514@gmail.com>
2025-01-31 11:09:00 +0530

    completed two sum problem

---
+++ Day-02-Stack_Queue_HashMap/src/hashmaps/twosumproblem/TwoSum.java
@@ -0,0 +1,32 @@
+package hashmaps.twosumproblem;
+
+
+import java.util.*;
+
+class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i}; // Return indices
+            }
+
+            map.put(nums[i], i); // Store the current number with its index
+        }
+
+        return new int[]{-1, -1}; // No solution found (should not happen)
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]); // Output: 0, 1
+    }
+}
+
+
