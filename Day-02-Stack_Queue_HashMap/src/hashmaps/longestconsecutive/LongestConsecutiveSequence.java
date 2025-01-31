c05e79085d22a00fbca1ff4135d6a3f862efff0a
Raghav-0007 <rmyadav514@gmail.com>
2025-01-31 11:03:52 +0530

    completed longest consicutive sequence

---
+++ Day-02-Stack_Queue_HashMap/src/hashmaps/longestconsecutive/LongestConsecutiveSequence.java
@@ -0,0 +1,36 @@
+package hashmaps.longestconsecutive;
+import java.util.*;
+public class LongestConsecutiveSequence {
+    public static int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int maxLength = 0;
+
+        for (int num : nums) {
+            // Start a sequence only if num-1 is NOT present
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentLength = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentLength++;
+                }
+
+                maxLength = Math.max(maxLength, currentLength);
+            }
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println("Longest Consecutive Sequence Length: " + longestConsecutive(nums));
+    }
+}
